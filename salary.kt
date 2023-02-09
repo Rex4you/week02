ebb7c97a16d9041f99357f342fd2315bef8e5ba2
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-08 22:04:28 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,13 @@
+{
+		String name = "Rexford Baffour-Gyimah";
+		int age = 25;
+		double annualPay = 99999.99999;
+		System.out.print("My name is ");
+		System.out.print(name);
+		System.out.print(", my age is ");
+		System.out.print(age);
+		System.out.print(" and I hope to earn $");
+		System.out.print(annualPay);
+		System.out.print(" per year.");
+	}
+	}
