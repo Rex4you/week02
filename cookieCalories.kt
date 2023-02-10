ea24f704b56b68c7da7750d62b4191af574cae35
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-10 06:08:32 -0600

    Create cookieCalories.kt

---
+++ cookieCalories.kt
@@ -0,0 +1,17 @@
+int main()
+{
+  int calory;
+  int biscuits;
+  
+  cout<<"enter programology cookies number you ate"<<end1;
+  cin>>biscuits;
+  
+  /* 40biscuits= 3000calory
+  so each biscuit contains 75 calory
+  to find calories multiply it with 75*/
+  
+  calory=biscuits*75
+  cout<<"you at " <<calory<<" calories hurrah programology"
+  <<end1;
+  
+}
