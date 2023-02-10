4c3b1908549c47030812e0a1819611138501cc9a
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-10 05:51:06 -0600

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,10 @@
+int main()
+{
+	double PoTS 		= .62,
+		   CompSales 	= 4600000;
+	int    ECSales		= CompSales * PoTS;
+
+	cout << "The East Coast division is predicted to generate $";
+	cout << ECSales << " in sales." << endl; 
+	return 0;
+}
