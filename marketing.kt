50b56a9eb2b51dcea9fe7baa05a3f3f50e501485
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-23 23:01:32 -0600

    Update marketing.kt

--- marketing.kt
+++ marketing.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
     var friendsCount = 500
-    var friendsCount >500
-
-    println("Adopt a Dog Today!")
-    OR
-    println("Buy Cat Food At 20% Off")
+    if (friendsCount > 500) {
+        println("Adopt a Dog Today!")
+    } else {
+        println("Buy Cat Food At 20% Off")
+    }
 }
