5a6d7448e51c970eac6290da69031689c2e8be6d
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-25 15:10:58 -0600

    Update cookieCalories.kt

--- cookieCalories.kt
+++ cookieCalories.kt
@@ -1,17 +1,8 @@
-int main()
-{
-  int calory;
-  int biscuits;
+fun  main(){
+  val caloriesPerCookie = 75
   
-  cout<<"enter programology cookies number you ate"<<end1;
-  cin>>biscuits;
-  
-  /* 40biscuits= 3000calory
-  so each biscuit contains 75 calory
-  to find calories multiply it with 75*/
-  
-  calory=biscuits*75
-  cout<<"you at " <<calory<<" calories hurrah programology"
-  <<end1;
-  
+  printlin("Enter the number of cookies eaten: ")
+  var cookiesEaten = readLine()!!.toInt()
+  var calorieIntake =cookiesEaten * caloriePerCookie
+  println("Your calorie intake was " + calorieIntake + " calories. ")
 }
