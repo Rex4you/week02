1397dcc1e423658bee665f8653d6c7430dd72346
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-25 15:29:44 -0600

    Update salesPrediction.kt

--- salesPrediction.kt
+++ salesPrediction.kt
@@ -1,10 +1,7 @@
-int main()
-{
-	double PoTS 		= .62,
-		   CompSales 	= 4600000;
-	int    ECSales		= CompSales * PoTS;
-
-	cout << "The East Coast division is predicted to generate $";
-	cout << ECSales << " in sales." << endl; 
-	return 0;
+fun main() {
+    val percent = .62
+    val salesTotal = 4600000
+    val salesPercent = salesTotal * percent
+    val format: NumberFormat = NumberFormat.getcurrencyInstance()
+    println(format.format(salesPercent))
 }
